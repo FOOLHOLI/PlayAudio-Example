679e6de3b91b8f196f88e7883536a55217e44039
iOSBeginner <jenhausu@icloud.com>
2014-10-20 10:15:22 +0800

    add new file

---
+++ PlayAudio/PlayAudio/SelectAudioTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelectAudioTableViewController.h
+//  PlayAudio
+//
+//  Created by 蘇健豪1 on 2014/10/20.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectAudioTableViewController : UITableViewController
+
+@end
---
+++ PlayAudio/PlayAudio/SelectAudioTableViewController.m
@@ -0,0 +1,92 @@
+//
+//  SelectAudioTableViewController.m
+//  PlayAudio
+//
+//  Created by 蘇健豪1 on 2014/10/20.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import "SelectAudioTableViewController.h"
+
+@interface SelectAudioTableViewController ()
+
+@property (nonatomic, strong) NSArray *array;
+
+@end
+
+@implementation SelectAudioTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.array = @[@"rain", @"Parasail"];
+}
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return self.array.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    cell.textLabel.text = self.array[indexPath.row];
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
