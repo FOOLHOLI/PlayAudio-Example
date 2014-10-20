a4a8ae7c89855a43842abf8c29f158287b95844d
iOSBeginner <jenhausu@icloud.com>
2014-10-20 09:30:57 +0800

    initialization

---
+++ PlayAudio/PlayAudio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayAudio
+//
+//  Created by 蘇健豪1 on 2014/10/20.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PlayAudio/PlayAudioTests/PlayAudioTests.m
@@ -0,0 +1,40 @@
+//
+//  PlayAudioTests.m
+//  PlayAudioTests
+//
+//  Created by 蘇健豪1 on 2014/10/20.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PlayAudioTests : XCTestCase
+
+@end
+
+@implementation PlayAudioTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
